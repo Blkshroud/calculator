d4e08715502c185e1a7817e9fe05084bd9a4acba
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-11-27 17:07:03 -0500

    Split Display function and added clearing ability -Display function split into Num, Operator, and Clear -Num handles and numbers and equals -Operator is called for / X + - -Clear is called initially to set the display and then any time Clear is clicked -Next will be to add a function for the decimal and clean up the entry logic.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,42 @@
 /* eslint-disable max-len */
 /* eslint-disable indent */
-let gNum1 = '';
-let gNum2 = '';
-let gOperator = '';
-let num1Done = false;
-let num2Done = false;
+let gNum1;
+let gNum2;
+let gOperator;
+let num1Done;
+let num2Done;
 const result = document.querySelector('#result');
 
 function addition(num1, num2) {
-    const answer = num1 + num2;
-    result.textContent = answer;
+    gNum1 = num1 + num2;
+    result.textContent = gNum1;
+    gNum2 = '';
+    gOperator = '';
+    num2Done = false;
 }
 
 function subtraction(num1, num2) {
-    const answer = num1 - num2;
-    result.textContent = answer;
+    gNum1 = num1 - num2;
+    result.textContent = gNum1;
+    gNum2 = '';
+    gOperator = '';
+    num2Done = false;
 }
 
 function multiplication(num1, num2) {
-    const answer = num1 * num2;
-    result.textContent = answer;
+    gNum1 = num1 * num2;
+    result.textContent = gNum1;
+    gNum2 = '';
+    gOperator = '';
+    num2Done = false;
 }
 
 function division(num1, num2) {
-    const answer = num1 / num2;
-    result.textContent = answer;
+    gNum1 = num1 / num2;
+    result.textContent = gNum1;
+    gNum2 = '';
+    gOperator = '';
+    num2Done = false;
 }
 
 function operate(num1, operator, num2) {
@@ -45,16 +57,9 @@ function operate(num1, operator, num2) {
     }
 }
 
-result.textContent = `0${gNum1}${gOperator}${gNum2}`;
-
-function display(entry) {
-    if (isNaN(entry.currentTarget.value) === false && num1Done === false) {
+function numDisplay(entry) {
+    if (num1Done === false) {
         gNum1 += entry.currentTarget.value;
-        result.textContent = `${gNum1}${gOperator}${gNum2}`;
-    }
-    else if (isNaN(entry.currentTarget.value) === true && gNum1 !== '' && num1Done === false) {
-        gOperator = entry.currentTarget.value;
-        num1Done = true;
         result.textContent = `${gNum1}${gOperator}${gNum2}`;
     }
     else if (isNaN(entry.currentTarget.value) === false && num1Done === true && num2Done === false) {
@@ -67,74 +72,91 @@ function display(entry) {
     }
 }
 
+function operDisplay(entry) {
+    gOperator = entry.currentTarget.value;
+    num1Done = true;
+    result.textContent = `${gNum1}${gOperator}${gNum2}`;
+}
+
+function clearDisplay() {
+    gNum1 = '';
+    gNum2 = '';
+    gOperator = '';
+    num1Done = false;
+    num2Done = false;
+    result.textContent = `0${gNum1}${gOperator}${gNum2}`;
+}
+
 const numBtn1 = document.querySelector('.one');
-numBtn1.addEventListener('click', display);
+numBtn1.addEventListener('click', numDisplay);
 numBtn1.value = 1;
 
 const numBtn2 = document.querySelector('.two');
-numBtn2.addEventListener('click', display);
+numBtn2.addEventListener('click', numDisplay);
 numBtn2.value = 2;
 
 const numBtn3 = document.querySelector('.three');
-numBtn3.addEventListener('click', display);
+numBtn3.addEventListener('click', numDisplay);
 numBtn3.value = 3;
 
 const numBtn4 = document.querySelector('.four');
-numBtn4.addEventListener('click', display);
+numBtn4.addEventListener('click', numDisplay);
 numBtn4.value = 4;
 
 const numBtn5 = document.querySelector('.five');
-numBtn5.addEventListener('click', display);
+numBtn5.addEventListener('click', numDisplay);
 numBtn5.value = 5;
 
 const numBtn6 = document.querySelector('.six');
-numBtn6.addEventListener('click', display);
+numBtn6.addEventListener('click', numDisplay);
 numBtn6.value = 6;
 
 const numBtn7 = document.querySelector('.seven');
-numBtn7.addEventListener('click', display);
+numBtn7.addEventListener('click', numDisplay);
 numBtn7.value = 7;
 
 const numBtn8 = document.querySelector('.eight');
-numBtn8.addEventListener('click', display);
+numBtn8.addEventListener('click', numDisplay);
 numBtn8.value = 8;
 
 const numBtn9 = document.querySelector('.nine');
-numBtn9.addEventListener('click', display);
+numBtn9.addEventListener('click', numDisplay);
 numBtn9.value = 9;
 
 const numBtn0 = document.querySelector('.zero');
-numBtn0.addEventListener('click', display);
+numBtn0.addEventListener('click', numDisplay);
 numBtn0.value = 0;
 
-const backspaceBtn = document.querySelector('.backspace');
-backspaceBtn.addEventListener('click', display);
-backspaceBtn.value = 'undo';
+// const backspaceBtn = document.querySelector('.backspace');
+// backspaceBtn.addEventListener('click', display);
+// backspaceBtn.value = 'undo';
 
 const clearBtn = document.querySelector('.clear');
-clearBtn.addEventListener('click', display);
+clearBtn.addEventListener('click', clearDisplay);
 clearBtn.value = 'clear';
 
 const multiBtn = document.querySelector('.multiplication');
-multiBtn.addEventListener('click', display);
+multiBtn.addEventListener('click', operDisplay);
 multiBtn.value = '×';
 
 const divBtn = document.querySelector('.division');
-divBtn.addEventListener('click', display);
+divBtn.addEventListener('click', operDisplay);
 divBtn.value = '/';
 
 const subBtn = document.querySelector('.subtraction');
-subBtn.addEventListener('click', display);
+subBtn.addEventListener('click', operDisplay);
 subBtn.value = '-';
 
 const addBtn = document.querySelector('.addition');
-addBtn.addEventListener('click', display);
+addBtn.addEventListener('click', operDisplay);
 addBtn.value = '+';
 
 const equalBtn = document.querySelector('.equals');
-equalBtn.addEventListener('click', display);
+equalBtn.addEventListener('click', numDisplay);
 equalBtn.value = '=';
 
-const decBtn = document.querySelector('.decimal');
-decBtn.addEventListener('click', display);
-decBtn.value = '.';
+// const decBtn = document.querySelector('.decimal');
+// decBtn.addEventListener('click', display);
+// decBtn.value = '.';
+
+clearDisplay();
